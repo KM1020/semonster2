abc99dd5ace2caae5d0dac1511deeac5036ee142
KM1020 <m1m20a41@oit.ac.jp>
2021-07-19 10:08:58 +0900

    MonsterとPlayerの追加

---
+++ app/src/main/java/semonster2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("SEMONSTER!");
+    Player p1 = new Player();
+    //Monster monster = new Monster();
+    p1.drawMonsters();
+    p1.showDeck();
+    //System.out.println(monster.toString());
+  }
+}
