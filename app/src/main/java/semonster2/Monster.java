14238d9fbfe91a8ca15ea6bf813b76f803362127
MoritaMichimasa <m1m20a47@oit.ac.jp>
2021-07-24 18:20:00 +0900

    レア度の追加

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -3,12 +3,12 @@ import java.util.Random;
 
 public class Monster {
   String name;
-  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
+  int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare,5:motoko rare,6:Moririn rare,7:Monkey rare
 
   Monster() {
     Random random = new Random();
     this.name = this.summonMonster(random.nextInt(5));
-    this.rare = random.nextInt(5);
+    this.rare = random.nextInt(8);
   }
 
   String summonMonster(int mnumber) {
